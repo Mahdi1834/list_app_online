4970f4baa33b6a56d63ecf1f9945fe0f200027f4
mahdi <mahdi.shabani1834@gmail.com>
2025-05-11 14:39:17 +0330

    finish add item to bottomsheet

--- lib/features/feature_item/presentation/screen/item_screen.dart
+++ lib/features/feature_item/presentation/screen/item_screen.dart
@@ -6,6 +6,7 @@ import 'package:list_app_online/features/feature_item/data/models/ItemModelParen
 import 'package:list_app_online/features/feature_item/presentation/bloc/delete_item_status.dart';
 import 'package:list_app_online/features/feature_item/presentation/bloc/get_all_item_tree_status.dart';
 import 'package:list_app_online/features/feature_item/presentation/bloc/item_bloc.dart';
+import 'package:list_app_online/features/feature_item/presentation/bloc/save_item_status.dart';
 import 'package:list_app_online/features/feature_item/presentation/screen/save_item_screen.dart';
 import 'package:list_app_online/features/feature_item_details/presentation/bloc/item_details_bloc.dart';
 import 'package:list_app_online/features/feature_item_details/presentation/screen/item_details_screen.dart';
@@ -51,7 +52,7 @@ class _ItemScreenState extends State<ItemScreen> {
             }
             if (state.getAllItemTreeStatus is GetAllItemTreeError) {
               GetAllItemTreeError getAllItemTreeError =
-                  state.getAllItemTreeStatus as GetAllItemTreeError;
+              state.getAllItemTreeStatus as GetAllItemTreeError;
               String? message = getAllItemTreeError.message;
 
               return Center(
@@ -86,13 +87,18 @@ class _ItemScreenState extends State<ItemScreen> {
 
             if (state.getAllItemTreeStatus is GetAllItemTreeCompleted) {
               GetAllItemTreeCompleted getAllItemTreeCompleted =
-                  state.getAllItemTreeStatus as GetAllItemTreeCompleted;
-              ItemModelParent itemModelParent = getAllItemTreeCompleted.itemModelParent;
+              state.getAllItemTreeStatus as GetAllItemTreeCompleted;
+              ItemModelParent itemModelParent = getAllItemTreeCompleted
+                  .itemModelParent;
 
               final items = itemModelParent.data?.items ?? [];
 
               return ListView(
-                children: items.map((item) => buildTree(item)).toList(),
+                children: items
+                    .asMap()
+                    .entries
+                    .map((entry) => buildTree(entry.value, index: entry.key))
+                    .toList(),
               );
             }
             return const SizedBox.shrink();
@@ -103,11 +109,14 @@ class _ItemScreenState extends State<ItemScreen> {
     );
   }
 
-  Widget buildTree(Item item, {int level = 0}) {
-    const Color baseColor = Colors.blueAccent;
+  Widget buildTree(Item item, {int level = 0, int index = 0}) {
+
     final double opacity =
-        (1 - (level * 0.1)).clamp(0.4, 1.0); // از پررنگ به کم‌رنگ
-    final Color levelColor = baseColor.withOpacity(opacity);
+    (1 - (level * 0.1)).clamp(0.4, 1.0); // از پررنگ به کم‌رنگ
+    final Color levelColor = (index % 2 == 0)
+        ? Colors.blueAccent.withOpacity(opacity)
+        : Colors.purple.withOpacity(opacity);
+
 
     return Card(
       elevation: 2,
@@ -136,12 +145,12 @@ class _ItemScreenState extends State<ItemScreen> {
                     arguments: item.id!);
               },
             ),
-            // IconButton(
-            //   icon: Icon(Icons.edit, color: Colors.orange),
-            //   onPressed: () {
-            //     // ویرایش آیتم
-            //   },
-            // ),
+            IconButton(
+              icon: const Icon(Icons.add, color: Colors.orange),
+              onPressed: () {
+                showAddItemBottomSheet(context, item.id!);
+              },
+            ),
             BlocConsumer<ItemBloc, ItemState>(
               listenWhen: (previous, current) {
                 if (previous.deleteItemStatus == current.deleteItemStatus) {
@@ -158,37 +167,40 @@ class _ItemScreenState extends State<ItemScreen> {
                 }
                 if (state.deleteItemStatus is DeleteItemError) {
                   DeleteItemError deleteItemError =
-                      state.deleteItemStatus as DeleteItemError;
+                  state.deleteItemStatus as DeleteItemError;
                   showFlushbar(context,
                       deleteItemError.message ?? "خطا هنگام حذف", false);
                 }
               },
               builder: (context, state) {
                 if (state.deleteItemStatus is DeleteItemLoading) {
-                  return DotLoadingWidget(size: 20);
+                  return const DotLoadingWidget(size: 20);
                 }
                 return IconButton(
                   icon: const Icon(Icons.delete, color: Colors.red),
                   onPressed: () async {
                     final confirm = await showDialog<bool>(
                       context: context,
-                      builder: (context) => AlertDialog(
-                        title: const Text('تأیید حذف'),
-                        content: const Text(
-                            'آیا از حذف این آیتم اطمینان دارید؟ تمام فرزندان و توضیحات آن نیز حذف خواهد شد.'),
-                        actions: [
-                          TextButton(
-                            onPressed: () => Navigator.of(context).pop(false),
-                            child: const Text('لغو'),
+                      builder: (context) =>
+                          AlertDialog(
+                            title: const Text('تأیید حذف'),
+                            content: const Text(
+                                'آیا از حذف این آیتم اطمینان دارید؟ تمام فرزندان و توضیحات آن نیز حذف خواهد شد.'),
+                            actions: [
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.of(context).pop(false),
+                                child: const Text('لغو'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () =>
+                                    Navigator.of(context).pop(true),
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.red),
+                                child: const Text('حذف'),
+                              ),
+                            ],
                           ),
-                          ElevatedButton(
-                            onPressed: () => Navigator.of(context).pop(true),
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.red),
-                            child: const Text('حذف'),
-                          ),
-                        ],
-                      ),
                     );
 
                     if (confirm == true) {
@@ -208,4 +220,103 @@ class _ItemScreenState extends State<ItemScreen> {
     );
   }
 // }
+
+
 }
+
+
+void showAddItemBottomSheet(BuildContext context, num parentId) {
+  final TextEditingController _titleController = TextEditingController();
+
+  showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+    ),
+    backgroundColor: Colors.white,
+    builder: (context) {
+      return Directionality(
+        textDirection: TextDirection.rtl,
+        child: Padding(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+            left: 16,
+            right: 16,
+            top: 24,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'افزودن آیتم جدید',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              TextField(
+                controller: _titleController,
+                decoration: const InputDecoration(
+                  labelText: 'عنوان',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton.icon(
+                onPressed: () {
+                  final newTitle = _titleController.text.trim();
+                  if (newTitle.isNotEmpty) {
+                    // ارسال event برای ایجاد آیتم جدید
+                    BlocProvider.of<ItemBloc>(context).add(
+                        SaveItemEvent(title: newTitle, parent_id: parentId.toString()));
+                    // Navigator.pop(context);
+                  }else{
+                    showFlushbar(context, "عنوان را وارد کنید",false);
+                  }
+                },
+                icon: const Icon(Icons.save),
+                label: const Text('ذخیره'),
+              ),
+              const SizedBox(height: 16),
+              BlocConsumer<ItemBloc, ItemState>(
+                listenWhen: (previous, current) {
+                  if (current.saveItemStatus == previous.saveItemStatus) {
+                    return false;
+                  }
+                  return true;
+                },
+                builder: (context, state) {
+                  if (state.saveItemStatus is SaveItemLoading) {
+                    return const DotLoadingWidget(size: 30);
+                  }
+                  return const SizedBox.shrink();
+                },
+                listener: (context, state) async {
+                  if (state.saveItemStatus is SaveItemCompleted) {
+                    await showFlushbar(
+                        context, "آیتم با موفقیت ذخیره شد", true);
+                    BlocProvider.of<ItemBloc>(context)
+                        .add(GetAllItemsTreeEvent());
+                    BlocProvider.of<ItemBloc>(context)
+                        .add(GetAllItemsEvent());
+                    // WidgetsBinding.instance.addPostFrameCallback((_) {
+                    Navigator.pop(context);
+                    // });
+                  }
+                  if (state.saveItemStatus is SaveItemError) {
+                    String? error =
+                        (state.saveItemStatus as SaveItemError).message;
+
+                    showFlushbar(
+                        context, error ?? "خطا در ذخیره‌سازی", false);
+                  }
+                },
+              )
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
+
+
